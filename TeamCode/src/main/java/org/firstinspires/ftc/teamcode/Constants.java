bc222f712efca46ee57ef923dd2fe53fc2bb4411
DJ Shabsifhe <zhangbaole111@126.com>
2024-11-14 15:13:15 +0800

    Initialize

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+    // TeleOp Mecanum Drive
+    public static double teleP = 0.2;
+    public static double teleI = 0;
+    public static double teleD = 0;
+    public static double teleF = 0;
+
+    public static double maxMotorAngularVelocity = 10;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/GamepadIO.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+public class GamepadIO {
+    private double leftY;
+    private double leftX;
+    private double rightY;
+    private double rightX;
+
+    public GamepadIO(double leftY, double leftX, double rightY, double rightX) {
+        this.leftY = leftY;
+        this.leftX = leftX;
+        this.rightY = rightY;
+        this.rightX = rightX;
+
+    }
+}
