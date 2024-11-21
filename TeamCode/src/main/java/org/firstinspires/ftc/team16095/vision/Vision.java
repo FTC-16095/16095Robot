cedd56b4670a751c2ce037284738441863030799
CarLeoNiu <146795913+CarLeoNiu@users.noreply.github.com>
2024-11-21 15:19:46 +0800

    Update Vision.java

--- TeamCode/src/main/java/org/firstinspires/ftc/team16095/vision/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team16095/vision/Vision.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.team16095.vision;
 
-public class Vision {
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.team16095.Robot;
+
+public class Vision extends LinearOpMode {
+    private Limelight3A limelight;
+    private LLResult lastResult;
+    private Robot robot;
+
+    public Vision(HardwareMap hardwareMap, Robot robot) {
+        this.limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        this.robot = robot;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        limelight.start();
+    }
+    
+    public LLResult getLastResult(){
+        return limelight.getLatestResult();
+    }
 }
